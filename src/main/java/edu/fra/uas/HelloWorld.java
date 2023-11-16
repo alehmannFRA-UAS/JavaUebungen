8498fa0171dff1ce03a92e2f4e69b037aa543919
alehmannFRA-UAS <88931807+alehmannFRA-UAS@users.noreply.github.com>
2023-11-16 09:28:35 +0000

    First example code

---
+++ src/main/java/edu/fra/uas/HelloWorld.java
@@ -0,0 +1,16 @@
+package edu.fra.uas;
+
+/**
+ * This is an exemplary class.
+ */
+public class HelloWorld {
+
+    /**
+     * A main method as first example
+     * @param args not supported
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");    
+    }
+
+}
---
+++ src/test/java/edu/fra/uas/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package edu.fra.uas;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldTest {
+
+    public HelloWorldTest() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+
+}
